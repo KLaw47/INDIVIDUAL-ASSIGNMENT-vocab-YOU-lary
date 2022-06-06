e6d2b890c91ebaaddfb6f8565031eb9555019c16
Keaton Law <keaton.c.law@gmail.com>
2022-06-06 18:29:20 -0500

    added basic api calls

--- src/api/vocabData.js
+++ src/api/vocabData.js
@@ -14,3 +14,42 @@ const vocabCards = (uid) => new Promise((resolve, reject) => {
     })
     .catch((error) => reject(error));
 });
+
+const createVocab = (vocabCard, uid) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/vocabcards.json`, vocabCard)
+    .then((response) => {
+      const payload = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/vocabcards/${response.data.name}.json`, payload)
+        .then(() => {
+          vocabCards(uid).then(resolve);
+        });
+    }).catch((error) => reject(error));
+});
+
+const getSingleCard = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/vocabcards/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
+
+const deleteCard = (uid) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/vocabcards/${uid}.json`)
+    .then(() => {
+      vocabCards(uid).then(resolve);
+    })
+    .catch((error) => reject(error));
+});
+
+const updateCard = (vocabCard) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/vocabcards/${vocabCard.firebaseKey}.json`, vocabCard)
+    .then(() => vocabCards(vocabCard.uid).then(resolve))
+    .catch(reject);
+});
+
+export {
+  vocabCards,
+  createVocab,
+  getSingleCard,
+  deleteCard,
+  updateCard,
+};
